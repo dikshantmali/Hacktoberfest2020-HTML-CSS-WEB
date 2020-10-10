07491549670c90a8bad494f49b289629419369ac
Musadiq <peermusadiq111@gmail.com>
2020-10-10 11:24:51 +0530

    added register and login portal in php, html, css

---
+++ register and login php/README.md
@@ -0,0 +1,4 @@
+# Login And Register Web Portal
+
+Login And Register Web Portal using php, html, mysql and css.
+It has features like register, login and change password.
---
+++ register and login php/index.php
@@ -0,0 +1,48 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  //	echo "you must login first";
+    //$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+      <p> <a href="changepassword.php" style="color: blue;">Change Password</a> </p>
+    <?php endif ?>
+</div>
+		
+</body>
+</html>
