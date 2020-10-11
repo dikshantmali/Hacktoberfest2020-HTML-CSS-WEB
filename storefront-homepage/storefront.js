ad66f27ab909c9c1b79511cb9da2c898e7721a8b
tbal <tbal@salesforce.com>
2020-10-11 19:24:17 -0400

    refactored to use scroll magic js

--- storefront-homepage/storefront.js
+++ storefront-homepage/storefront.js
@@ -1,83 +1,76 @@
-let scroll_position = 0;
-let scroll_direction;
-let count = 1;
-let index = 0;
-let opacityValue = 0;
-let radius = 0;
-let width = 100;
-let margintop = 0;
+// init controller
+var controller = new ScrollMagic.Controller();
 
-window.onload = function () {
-    document.getElementsByTagName('body')[0].onscroll = () => {
-        //let top = window.document.documentElement.scrollTop;
-        let clientRect = (document.body.getBoundingClientRect());
-        let wbanner = document.getElementsByClassName("w-banner")[0];
-        let rlabel = document.getElementsByClassName("cat-label")[0];
-        let pBlock = document.getElementsByClassName("p-block");
-        let length = pBlock.length;
-        scroll_direction = clientRect.top > scroll_position ? 0 : 1;
-        scroll_position = clientRect.top;
-        // Reset at top
-        for (let i = 0; i < length; i++) {
-            if (clientRect.top === 0) {
-                if (i == 0) {
-                    pBlock[i].setAttribute("style", "opacity:1");
-                } else {
-                    pBlock[i].setAttribute("style", "opacity:0");
-                }
-            }
-            
-        }
+new ScrollMagic.Scene({
+    triggerElement: "#trigger1",
+    triggerHook: 0.8, // show, when scrolled 10% into view
+    duration: "90%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+    .setClassToggle("#reveal1", "visible") // add class to reveal
+    .addTo(controller);
 
-        // Find the active block
-        for (let i = 0; i < length; i++) {
-            opacityValue = parseFloat(pBlock[i].getAttribute('style').split(":")[1]);
-            if (opacityValue != 0) {
-                index = i;
-                break;
-            }
-        }
+new ScrollMagic.Scene({
+    triggerElement: "#trigger2",
+    triggerHook: 0.8, // show, when scrolled 10% into view
+    duration: "90%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+    .setClassToggle("#reveal2", "visible") // add class to reveal
+    .addTo(controller);
 
-        // up scroll
-        if (scroll_direction === 0) {
-            if (opacityValue == 1 && index != 0) {
-                pBlock[index].setAttribute("style", "opacity:0");
-                index = index - 1;
-                opacityValue = 0;
-            }
-            opacityValue = opacityValue + 0.01;
-            if (opacityValue > 1) opacityValue = 1;
-            pBlock[index].setAttribute("style", "opacity:" + opacityValue);
+new ScrollMagic.Scene({
+    triggerElement: "#trigger3",
+    triggerHook: 0.8, // show, when scrolled 10% into view
+    duration: "90%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+    .setClassToggle("#reveal3", "visible") // add class to reveal
+    .addTo(controller);
 
-            radius = radius - 1;
-            margintop = margintop - 1;
-            width = width + 1;
-            if (radius < 0) radius = 0;
-            if (margintop < 0) margintop = 0;
-            if (width > 100) width = 100;
-            wbanner.setAttribute("style", "border-radius:" + radius + "%;" + "width:" + width + "%");
-            rlabel.setAttribute("style", "margin-top:-" + margintop + "vh");
-        }
-        else { // down scroll
-            if (opacityValue == 0 && pBlock[index + 1] != null) {
-                index = index + 1;
-                opacityValue = 1;
-            }
-            opacityValue = opacityValue - 0.015;
-            if (opacityValue < 0) opacityValue = 0;
-            pBlock[index].setAttribute("style", "opacity:" + opacityValue);
-            if ((wbanner.getBoundingClientRect()).top < 500) {
-                radius = radius + 1;
-                margintop = margintop + 1;
-                width = width - 1;
-                if (radius > 50) radius = 50;
-                if (width < 50) width = 50;
-                if (margintop > 10) margintop = 10;
-                wbanner.setAttribute("style", "border-radius:" + radius + "%;" + "width:" + width + "%");
-                rlabel.setAttribute("style", "margin-top:-" + margintop + "vh");
-            }
+new ScrollMagic.Scene({
+    triggerElement: "#trigger4",
+    triggerHook: 0.8, // show, when scrolled 10% into view
+    duration: "90%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+    .setClassToggle("#reveal4", "visible") // add class to reveal
+    .addTo(controller);
 
-        }
+var pinAnimation = new TimelineMax()
+    .fromTo("section.panel.pin", 1, { x: "100%" }, { x: "0%", ease: Linear.easeNone })  // in from left
+// create scene to pin and link animation
+new ScrollMagic.Scene({
+    triggerElement: "#pinContainer",
+    triggerHook: "onLeave",
+    duration: "500%"
+})
+    .setPin("#pinContainer")
+    .setTween(pinAnimation)
+    .addTo(controller);
 
-    }
-}
+
+// define movement of panels
+var wipeAnimation = new TimelineMax()
+    // animate to second panel
+    .to("#slideContainer", 0.5, { z: -150 })		// move back in 3D space
+    .to("#slideContainer", 1, { x: "-25%" })	// move in to first panel
+    .to("#slideContainer", 0.5, { z: 0 })			// move back to origin in 3D space
+    // animate to third panel
+    .to("#slideContainer", 0.5, { z: -150, delay: 1 })
+    .to("#slideContainer", 1, { x: "-50%" })
+    .to("#slideContainer", 0.5, { z: 0 })
+    // animate to forth panel
+    .to("#slideContainer", 0.5, { z: -150, delay: 1 })
+    .to("#slideContainer", 1, { x: "-75%" })
+    .to("#slideContainer", 0.5, { z: 0 });
+
+// create scene to pin and link animation
+new ScrollMagic.Scene({
+    triggerElement: "#pinContainer1",
+    triggerHook: "onLeave",
+    duration: "500%"
+})
+    .setPin("#pinContainer1")
+    .setTween(wipeAnimation)
+    .addTo(controller);
